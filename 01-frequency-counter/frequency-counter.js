c7256da9384ae616f2a832dcc4a9176a2e3574b8
xaviguasch <xaviguasch@gmail.com>
2020-06-22 20:02:55 +0200

    added alternative solution

--- 01-frequency-counter/frequency-counter.js
+++ 01-frequency-counter/frequency-counter.js
@@ -1,15 +1,31 @@
+// function same(arr1, arr2) {
+//   const result = arr1.map((number) => {
+//     const squared = number * number
+
+//     if (arr2.includes(squared)) {
+//       return true
+//     }
+//   })
+
+//   return result.every((st) => st === true)
+// }
+
+// console.log(same([2, 3, 4], [9, 4, 16]))
+
+// Write a function called same, which accepts two arrays. The function should return true if every value in the array has it's corresponding value squared in the second array. The frequency of values must be the same.
+
 function same(arr1, arr2) {
-  const result = arr1.map((number) => {
-    const squared = number * number
-
-    if (arr2.includes(squared)) {
-      return true
+  if (arr1.length !== arr2.length) {
+    return false
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    let correctIndex = arr2.indexOf(arr1[i] ** 2)
+    if (correctIndex === -1) {
+      return false
     }
-  })
-
-  return result.every((st) => st === true)
+    arr2.splice(correctIndex, 1)
+  }
+  return true
 }
 
 console.log(same([2, 3, 4], [9, 4, 16]))
-
-// Write a function called same, which accepts two arrays. The function should return true if every value in the array has it's corresponding value squared in the second array. The frequency of values must be the same.
