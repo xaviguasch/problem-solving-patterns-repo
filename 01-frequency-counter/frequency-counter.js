e9192c253015199c5889adab25e9e3e96d57a3a0
xaviguasch <xaviguasch@gmail.com>
2020-06-22 21:04:32 +0200

    added refactored solution with O(n)

--- 01-frequency-counter/frequency-counter.js
+++ 01-frequency-counter/frequency-counter.js
@@ -14,16 +14,43 @@
 
 // Write a function called same, which accepts two arrays. The function should return true if every value in the array has it's corresponding value squared in the second array. The frequency of values must be the same.
 
+// function same(arr1, arr2) {
+//   if (arr1.length !== arr2.length) {
+//     return false
+//   }
+//   for (let i = 0; i < arr1.length; i++) {
+//     let correctIndex = arr2.indexOf(arr1[i] ** 2)
+//     if (correctIndex === -1) {
+//       return false
+//     }
+//     arr2.splice(correctIndex, 1)
+//   }
+//   return true
+// }
+
+// console.log(same([2, 3, 4], [9, 4, 16]))
+
+// refactored solution with O(n)
+
 function same(arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false
   }
-  for (let i = 0; i < arr1.length; i++) {
-    let correctIndex = arr2.indexOf(arr1[i] ** 2)
-    if (correctIndex === -1) {
+  let frequencyCounter1 = {}
+  let frequencyCounter2 = {}
+  for (let val of arr1) {
+    frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1
+  }
+  for (let val of arr2) {
+    frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1
+  }
+  for (let key in frequencyCounter1) {
+    if (!(key ** 2 in frequencyCounter2)) {
       return false
     }
-    arr2.splice(correctIndex, 1)
+    if (frequencyCounter2[key ** 2] !== frequencyCounter1[key]) {
+      return false
+    }
   }
   return true
 }
